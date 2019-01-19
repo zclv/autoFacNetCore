9de020beec77ab56d9942e6486e16e2db9b4fa31
zclv <996454914@qq.com>
2019-01-19 21:06:40 +0800

    中间件

--- writeIpMiddleWare/Startup.cs
+++ writeIpMiddleWare/Startup.cs
@@ -17,6 +17,7 @@ namespace writeIpMiddleWare
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSession();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,6 +42,7 @@ namespace writeIpMiddleWare
 
             }));
             loggerFactory.AddConsole(minLevel:LogLevel.Information);
+            app.UseSession();
             app.UseRequestIP();//使用中间件
             app.UseStaticFiles();//使用静态文件中间件
             app.Run(async (context) =>
